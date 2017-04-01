58873b8688a16cd9ba1f5ff43da1608636a1733d
Raghav <prasanna.gn6@gmail.com>
2017-04-01 12:10:44 +0530

    Setup solution

---
+++ WebApplication1/WebApplication1/Hackathon.Data/Candidate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Hackathon.Data
+{
+    public class Candidate
+    {
+        public Guid CandiateId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        //public Address Address { get; set; }
+    }
+}
